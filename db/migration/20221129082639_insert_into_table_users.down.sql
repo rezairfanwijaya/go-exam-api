7a551d5ba479ee21725161573bfc2f68fa303c5d
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-11-29 16:02:35 +0700

    add: migration user

---
+++ db/migration/20221129082639_insert_into_table_users.down.sql
@@ -0,0 +1,5 @@
+DELETE from users where email = "siswapertama@gmail.com";
+DELETE from users where email = "siswakedua@gmail.com";
+DELETE from users where email = "siswaketiga@gmail.com";
+DELETE from users where email = "siswakeempat@gmail.com";
+DELETE from users where email = "guru@gmail.com";
---
+++ db/migration/20221129082639_insert_into_table_users.up.sql
@@ -0,0 +1,8 @@
+INSERT INTO users 
+    (full_name, email, password, role)
+VALUES
+    ("siswa pertama", "siswapertama@gmail.com", "12345678", "siswa"),
+    ("siswa kedua", "siswakedua@gmail.com", "12345678", "siswa"),
+    ("siswa ketiga", "siswaketiga@gmail.com", "12345678", "siswa"),
+    ("siswa keempat", "siswakeempat@gmail.com", "12345678", "siswa"),
+    ("guru", "guru@gmail.com", "12345678", "guru");
