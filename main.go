9c2973c8542f7f14974236e03a5589f4fdb22ecf
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-11-30 00:19:01 +0700

    refactor: route

--- main.go
+++ main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"log"
 
+	"github.com/labstack/echo/v4"
 	"github.com/rezairfanwijaya/go-exam-api.git/connection"
+	"github.com/rezairfanwijaya/go-exam-api.git/route"
 )
 
 func main() {
-	DBConnecton, err := connection.DB(".env")
+	// create connection to database
+	dbConnection, err := connection.DB(".env")
 	if err != nil {
 		log.Println(err)
 	}
 
-	log.Println(DBConnecton)
+	// define echo
+	e := echo.New()
+
+	// define route
+	route.NewRoute(e, dbConnection)
+
+	// start server echo
+	if err := e.Start("localhost:9090"); err != nil {
+		log.Println(err)
+	}
 }
