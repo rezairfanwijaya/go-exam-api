9d24244dda2e36d5bd68a3844705eba3447edffb
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-01 22:01:23 +0700

    update: injection auth service to handler

--- route/route.go
+++ route/route.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
 	"github.com/rezairfanwijaya/go-exam-api.git/handler"
+	"github.com/rezairfanwijaya/go-exam-api.git/middleware"
 	"github.com/rezairfanwijaya/go-exam-api.git/user"
 	"gorm.io/gorm"
 )
@@ -22,12 +23,15 @@ func NewRoute(e *echo.Echo, db *gorm.DB) {
 	// set validator
 	e.Validator = &CustomValidator{validator: validator.New()}
 
+	// service auth
+	serviceAuth := middleware.NewServiceAuth()
+
 	// user repo
 	repoUser := user.NewRepository(db)
 	// user service
 	serviceUser := user.NewService(repoUser)
 	// user handler
-	handlerUser := handler.NewHandler(serviceUser)
+	handlerUser := handler.NewHandler(serviceUser, serviceAuth)
 
 	e.POST("/login", handlerUser.Login)
 }
