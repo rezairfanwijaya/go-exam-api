a3e6b0cd4e67ead374be7869aed9e1b7451929c4
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-01 11:39:50 +0700

    add: user formating and response api formatter

--- helper/helper.go
+++ helper/helper.go
@@ -6,7 +6,26 @@ import (
 
 	"github.com/go-playground/validator"
 	"github.com/joho/godotenv"
+	"github.com/rezairfanwijaya/go-exam-api.git/user"
 )
+
+type responseAPI struct {
+	Meta meta        `json:"meta"`
+	Data interface{} `json:"data"`
+}
+
+type meta struct {
+	Status  string `json:"status"`
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+type userFormating struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Role  string `json:"role"`
+}
 
 // function get env value
 func GetEnv(path string) (map[string]string, error) {
@@ -28,4 +47,28 @@ func FormatErrorValidate(err error) []string {
 	}
 
 	return myErr
+}
+
+// func response api
+func ResponseAPI(status, message string, code int, data interface{}) responseAPI {
+	meta := meta{
+		Status:  status,
+		Code:    code,
+		Message: message,
+	}
+
+	return responseAPI{
+		Meta: meta,
+		Data: data,
+	}
+}
+
+// user fortmating
+func UserFormating(user user.User) userFormating {
+	return userFormating{
+		ID:    user.ID,
+		Name:  user.FullName,
+		Email: user.Email,
+		Role:  user.Role,
+	}
 }
