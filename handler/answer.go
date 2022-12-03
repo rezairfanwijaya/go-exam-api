a09d920bdafb0418bd336edab95343d1a995b3d4
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-03 22:31:20 +0700

    update: answer handler

--- handler/answer.go
+++ handler/answer.go
@@ -1,22 +1,34 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/rezairfanwijaya/go-exam-api.git/answer"
 	"github.com/rezairfanwijaya/go-exam-api.git/helper"
 	m "github.com/rezairfanwijaya/go-exam-api.git/middleware"
+	"github.com/rezairfanwijaya/go-exam-api.git/question"
 )
 
 type AnswerHandler struct {
-	answerService answer.IAnswerService
-	authService   m.IAuthService
+	answerService   answer.IAnswerService
+	authService     m.IAuthService
+	questionService question.IQuestionService
 }
 
-func NewHanlderAnswer(answerService answer.IAnswerService, authService m.IAuthService) *AnswerHandler {
-	return &AnswerHandler{answerService, authService}
+func NewHanlderAnswer(
+	answerService answer.IAnswerService,
+	authService m.IAuthService,
+	questionService question.IQuestionService,
+) *AnswerHandler {
+	return &AnswerHandler{
+		answerService,
+		authService,
+		questionService}
 }
+
+const SISWA = "siswa"
 
 // hanlder untuk menyimpan soal
 func (h *AnswerHandler) Save(c echo.Context) error {
@@ -47,8 +59,47 @@ func (h *AnswerHandler) Save(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, response)
 	}
 
+	// cek apakah id question tesedia
+	for _, questionDetail := range input.Answers {
+		question, _ := h.questionService.GetByID(questionDetail.QuestionID)
+		if question.ID == 0 {
+			errMsg := fmt.Sprintf("question dengan id %v tidak ditemukan", questionDetail.QuestionID)
+			response := helper.ResponseAPI(
+				"gagal",
+				"gagal submit jawaban",
+				http.StatusBadRequest,
+				errMsg,
+			)
+
+			return c.JSON(http.StatusBadRequest, response)
+		}
+	}
+
+	// yang bisa menjawab ujian hanya siswa saja
+	role, id := helper.AuthRole(c)
+	if role != SISWA {
+		response := helper.ResponseAPI(
+			"Unauthorized",
+			"error",
+			http.StatusUnauthorized,
+			"akses ditolak, hanya siswa yang dapat mengisi jawaban",
+		)
+
+		return c.JSON(http.StatusUnauthorized, response)
+	}
+
 	// panggil service
-	h.answerService.Save(input)
+	err := h.answerService.Save(input, id)
+	if err != nil {
+		response := helper.ResponseAPI(
+			"gagal",
+			"gagal submit jawaban",
+			http.StatusBadRequest,
+			err.Error(),
+		)
+
+		return c.JSON(http.StatusBadRequest, response)
+	}
 
 	response := helper.ResponseAPI(
 		"sukses",
