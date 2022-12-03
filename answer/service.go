2364749159668309edba666d43b99e0fe482e0d5
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-03 22:29:41 +0700

    update: answer service

--- answer/service.go
+++ answer/service.go
@@ -1,28 +1,51 @@
 package answer
 
+import (
+	"errors"
+	"fmt"
+)
+
 type IAnswerService interface {
-	Save(inputs Answers) error
+	Save(inputs Answers, userID int) error
+	GetByUserID(userID int) ([]Answer, error)
 }
 
 type AnswerService struct {
-	anserRepo IAnswerRepository
+	answerRepo IAnswerRepository
 }
 
-func NewService(anserRepo IAnswerRepository) *AnswerService {
-	return &AnswerService{anserRepo}
+func NewService(answerRepo IAnswerRepository) *AnswerService {
+	return &AnswerService{answerRepo}
 }
 
 // implementasi
-func (s *AnswerService) Save(inputs Answers) error {
+func (s *AnswerService) Save(inputs Answers, userID int) error {
+	// question deengan id yang sama tidak boleh di jawab dua kali oleh user yang sama
+	answersByUserID, _ := s.answerRepo.FindByUserID(userID)
+	for _, answerByUserID := range answersByUserID {
+		for _, input := range inputs.Answers {
+			if answerByUserID.UserID == userID && input.QuestionID == answerByUserID.QuestionID {
+				errMsg := fmt.Sprintf("question dengan id %v sudah dijawab", input.QuestionID)
+				return errors.New(errMsg)
+			}
+		}
+	}
+
 	for _, input := range inputs.Answers {
 		var answer Answer
 		answer.Answer = input.Answer
 		answer.QuestionID = input.QuestionID
+		answer.UserID = userID
 
-		func(answer Answer) {
-			s.anserRepo.Save(answer)
+		go func(answer Answer) {
+			s.answerRepo.Save(answer)
 		}(answer)
 	}
 
 	return nil
+}
+
+func (s *AnswerService) GetByUserID(userID int) ([]Answer, error) {
+	answers, _ := s.answerRepo.FindByUserID(userID)
+	return answers, nil
 }
