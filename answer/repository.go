443b6e1bb7b0251c54b13b377d2c378d2b82ac56
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-03 17:59:08 +0700

    add: answer repository

--- answer/repository.go
+++ answer/repository.go
@@ -1,2 +1,24 @@
 package answer
 
+import "gorm.io/gorm"
+
+type IAnswerRepository interface {
+	Save(answer Answer) error
+}
+
+type AnswerRepository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *AnswerRepository {
+	return &AnswerRepository{db}
+}
+
+// implementasi
+func (r *AnswerRepository) Save(answer Answer) error {
+	if err := r.db.Create(&answer).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
