6242b7bc192dfaf326fcc541598ff0f734e1feb2
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-03 23:39:45 +0700

    add: handler get all answer and get answer by user id

--- handler/answer.go
+++ handler/answer.go
@@ -28,7 +28,9 @@ func NewHanlderAnswer(
 		questionService}
 }
 
-const SISWA = "siswa"
+const (
+	SISWA = "siswa"
+)
 
 // hanlder untuk menyimpan soal
 func (h *AnswerHandler) Save(c echo.Context) error {
@@ -106,6 +108,68 @@ func (h *AnswerHandler) Save(c echo.Context) error {
 		"sukses menyimpan jawaban",
 		http.StatusOK,
 		"sukses",
+	)
+
+	return c.JSON(http.StatusOK, response)
+}
+
+func (h *AnswerHandler) GetAnswerByUserID(c echo.Context) error {
+	// yang bisa menjawab ujian hanya siswa saja
+	role, id := helper.AuthRole(c)
+	if role != SISWA {
+		response := helper.ResponseAPI(
+			"Unauthorized",
+			"error",
+			http.StatusUnauthorized,
+			"akses ditolak, hanya siswa yang diperbolehkan",
+		)
+
+		return c.JSON(http.StatusUnauthorized, response)
+	}
+
+	answers, err := h.answerService.GetByUserID(id)
+	if err != nil {
+		response := helper.ResponseAPI(
+			"gagal",
+			"gagal submit jawaban",
+			http.StatusBadRequest,
+			err.Error(),
+		)
+
+		return c.JSON(http.StatusBadRequest, response)
+	}
+
+	response := helper.ResponseAPI(
+		"sukses",
+		"sukses mengambil jawaban",
+		http.StatusOK,
+		answers,
+	)
+
+	return c.JSON(http.StatusOK, response)
+}
+
+func (h *AnswerHandler) GetAllAnswer(c echo.Context) error {
+	// hanya guru yang bisa melihat semua jawaban
+	role, _ := helper.AuthRole(c)
+
+	if role != GURU {
+		response := helper.ResponseAPI(
+			"Unauthorized",
+			"error",
+			http.StatusUnauthorized,
+			"akses ditolak, hanya guru yang diperbolehkan",
+		)
+
+		return c.JSON(http.StatusUnauthorized, response)
+	}
+
+	answers := h.answerService.GetAllAnswers()
+	response := helper.ResponseAPI(
+		"sukses",
+		"sukses mengambil semua jawaban",
+		http.StatusOK,
+		answers,
 	)
 
 	return c.JSON(http.StatusOK, response)
