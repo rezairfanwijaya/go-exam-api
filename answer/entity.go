05b2285d711148cddf884532d84dd6c08a1ebb98
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-03 22:28:48 +0700

    udpate: models answers

--- answer/entity.go
+++ answer/entity.go
@@ -4,4 +4,5 @@ type Answer struct {
 	ID         int    `json:"id" gorm:"primaryKey"`
 	Answer     string `json:"answer"`
 	QuestionID int    `json:"question_id"`
+	UserID     int    `json:"user_id"`
 }
