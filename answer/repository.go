eb97300a9df646bb9f09db35c42fa6211bc8a2e1
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-03 23:39:00 +0700

    update: answer repository

--- answer/repository.go
+++ answer/repository.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type IAnswerRepository interface {
 	Save(answer Answer) error
 	FindByUserID(userID int) ([]Answer, error)
+	FindAll() ([]Answer, error)
 }
 
 type AnswerRepository struct {
@@ -26,7 +27,18 @@ func (r *AnswerRepository) Save(answer Answer) error {
 
 func (r *AnswerRepository) FindByUserID(userID int) ([]Answer, error) {
 	var answers []Answer
+	
 	if err := r.db.Where("user_id = ?", userID).Find(&answers).Error; err != nil {
+		return answers, err
+	}
+
+	return answers, nil
+}
+
+func (r *AnswerRepository) FindAll() ([]Answer, error) {
+	var answers []Answer
+
+	if err := r.db.Find(&answers).Error; err != nil {
 		return answers, err
 	}
 
