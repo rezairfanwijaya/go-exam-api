9ba984828126eac71b4cc8f640b88b667efb7ecf
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-12-04 12:01:51 +0700

    add: answer repository

--- answer/repository.go
+++ answer/repository.go
@@ -6,6 +6,8 @@ type IAnswerRepository interface {
 	Save(answer Answer) error
 	FindByUserID(userID int) ([]Answer, error)
 	FindAll() ([]Answer, error)
+	FindByUserIDAndQuestionID(questionID, userID int) (Answer, error)
+	DeleteByQuestionID(questionID, userID int) (Answer, error)
 }
 
 type AnswerRepository struct {
@@ -27,7 +29,7 @@ func (r *AnswerRepository) Save(answer Answer) error {
 
 func (r *AnswerRepository) FindByUserID(userID int) ([]Answer, error) {
 	var answers []Answer
-	
+
 	if err := r.db.Where("user_id = ?", userID).Find(&answers).Error; err != nil {
 		return answers, err
 	}
@@ -43,4 +45,24 @@ func (r *AnswerRepository) FindAll() ([]Answer, error) {
 	}
 
 	return answers, nil
+}
+
+func (r *AnswerRepository) FindByUserIDAndQuestionID(questionID, userID int) (Answer, error) {
+	var answer Answer
+
+	if err := r.db.Where("question_id = ? AND user_id = ? ", questionID, userID).Find(&answer).Error; err != nil {
+		return answer, err
+	}
+
+	return answer, nil
+}
+
+func (r *AnswerRepository) DeleteByQuestionID(questionID, userID int) (Answer, error) {
+	var answer Answer
+
+	if err := r.db.Where("question_id = ? AND user_id = ? ", questionID, userID).Delete(&answer).Error; err != nil {
+		return answer, err
+	}
+
+	return answer, nil
 }
